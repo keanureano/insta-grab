850d32e4b527ca0e7235be5c76c7d2df562eacc1
Keanu Reaño <reanokeanu@gmail.com>
2024-09-10 10:28:03 +0800

    Added functionality to use puppeteer to scrape an Instagram page in the index.js file. Updated the package.json file to include a "dev" script using nodemon for development and a "start" script. Minor updates to pnpm-lock.yaml file for nodemon as a dev dependency.

---
+++ index.js
@@ -0,0 +1,9 @@
+import puppeteer from "puppeteer";
+
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://www.instagram.com/nodejs.tech/");
+}
+
+main();
