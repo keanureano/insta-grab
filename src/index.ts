d9305edee925a0f12eac21ba4845c93808cf019e
Keanu Reaño <reanokeanu@gmail.com>
2024-09-10 11:41:17 +0800

    Updated the package.json file to change the main script path to "dist/index.js" and updated scripts for development, building, and starting the application. Updated dependencies and devDependencies versions, including adding TypeScript and ts-node. Also updated the pnpm-lock.yaml file to reflect the changes in dependencies versions and added new dependencies like acorn and diff.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import puppeteer from "puppeteer";
 async function main() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.instagram.com/nodejs.tech/");
+  await page.goto("https://www.instagram.com/nodejs.tech/", {
+    waitUntil: "networkidle2",
+  });
 }
 
 main();
