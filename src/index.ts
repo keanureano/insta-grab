bf1765ae984761712d5f43d669e69e8fee67fa25
Keanu Reaño <reanokeanu@gmail.com>
2024-09-10 13:25:36 +0800

    Updated dependencies in package.json and pnpm-lock.yaml - In package.json, added new dependencies "puppeteer-extra", "puppeteer-extra-plugin-stealth" - Removed "nodemon" from dependencies in package.json - Updated devDependencies versions in package.json - In pnpm-lock.yaml, added entries for the new dependencies with their respective versions

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,53 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
-async function main() {
-  const browser = await puppeteer.launch({ headless: false });
+puppeteer.use(StealthPlugin());
+puppeteer.launch({ headless: false, slowMo: 50 }).then(async (browser) => {
   const page = await browser.newPage();
 
   try {
     await page.goto("https://www.instagram.com/nodejs.tech/", {
       waitUntil: "networkidle2",
     });
-    await page.waitForSelector("section > div > div > h2", { timeout: 10000 });
 
-    const { username, picture } = await page.evaluate(() => {
-      const usernameElement = document.querySelector(
-        "section > div > div > h2"
-      );
-      const pictureElement = document.querySelector(
-        "header > div > div > span > img"
-      ) as HTMLImageElement;
-
-      return {
-        username: usernameElement ? usernameElement.textContent?.trim() : null,
-        picture: pictureElement ? pictureElement.src : null,
-      };
+    await page.waitForSelector("section > div > div > h2", {
+      timeout: 10000,
     });
 
+    const username = await page.evaluate(() => {
+      const element = document.querySelector(
+        "section > div > div > h2"
+      ) as HTMLElement;
+      return element ? element.textContent : null;
+    });
+
+    const profilePic = await page.evaluate(() => {
+      const element = document.querySelector(
+        "header > div > div > span > img"
+      ) as HTMLImageElement;
+      return element ? element.src : null;
+    });
+
+    // const posts = await page.$$("article > div > div > a");
+
+    // const likes = [];
+
+    // for (const post in posts) {
+    //   await page.hover(post);
+
+    //   const like = await page.evaluate(() => {
+    //     const element = document.querySelector("a > div > ul > li > span");
+    //     return element ? element.textContent : null;
+    //   });
+
+    //   likes.push(like);
+    // }
+
+    // console.log("Likes:", likes);
     console.log("Username:", username);
-    console.log("Picture:", picture);
+    console.log("Profile Picture:", profilePic);
   } catch (error) {
     console.error("Error:", error);
   } finally {
-    await browser.close();
   }
-}
-
-main();
+});
