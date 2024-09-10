1ba974da5fae24f1ce05d1ab055e9c1dac2aefe1
Keanu Reaño <reanokeanu@gmail.com>
2024-09-10 12:26:31 +0800

    Refactor code in src/index.ts to improve error handling and variable declaration. Add destructuring assignment for username and picture. Remove unnecessary variable declarations and improve code readability.

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,33 @@ import puppeteer from "puppeteer";
 async function main() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.instagram.com/nodejs.tech/", {
-    waitUntil: "networkidle2",
-  });
 
   try {
+    await page.goto("https://www.instagram.com/nodejs.tech/", {
+      waitUntil: "networkidle2",
+    });
     await page.waitForSelector("section > div > div > h2", { timeout: 10000 });
 
-    const username = await page.evaluate(() => {
-      const element = document.querySelector(
+    const { username, picture } = await page.evaluate(() => {
+      const usernameElement = document.querySelector(
         "section > div > div > h2"
-      ) as HTMLElement;
-      return element ? element.textContent : null;
-    });
-
-    const profilePic = await page.evaluate(() => {
-      const element = document.querySelector(
-        "header > div > div> span > img"
+      );
+      const pictureElement = document.querySelector(
+        "header > div > div > span > img"
       ) as HTMLImageElement;
-      return element ? element.src : null;
+
+      return {
+        username: usernameElement ? usernameElement.textContent?.trim() : null,
+        picture: pictureElement ? pictureElement.src : null,
+      };
     });
 
     console.log("Username:", username);
-    console.log("Profile Picture:", profilePic);
+    console.log("Picture:", picture);
   } catch (error) {
     console.error("Error:", error);
   } finally {
+    await browser.close();
   }
 }
 
