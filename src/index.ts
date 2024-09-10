396c3886c48083203a84b0f8ec62fb01a42b42c4
Keanu Reaño <reanokeanu@gmail.com>
2024-09-10 12:04:35 +0800

    Add dist folder to gitignore, add function to freeze execution for given time, and update tsconfig to include outDir and rootDir settings.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import puppeteer from "puppeteer";
+function freeze(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 async function main() {
   const browser = await puppeteer.launch({ headless: false });
@@ -6,6 +9,21 @@ async function main() {
   await page.goto("https://www.instagram.com/nodejs.tech/", {
     waitUntil: "networkidle2",
   });
+  try {
+    // Wait for the <h2> element to appear
+    await page.waitForSelector("h2", { timeout: 10000 }); // Wait for up to 10 seconds
+
+    // Extract the text content of the <h2> element
+    const username = await page.evaluate(() => {
+      const element = document.querySelector("h2");
+      return element ? element.textContent : null;
+    });
+
+    console.log("Username:", username);
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+  }
 }
 
 main();
